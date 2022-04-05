dcea45c4efef52241a800a3f40519c7d36329b86
João Carlos <market4u.dev6@gmail.com>
2022-04-05 14:59:35 -0300

    refactor: new line in functions

--- src/main/middlewares/content-type.test.ts
+++ src/main/middlewares/content-type.test.ts
@@ -18,6 +18,8 @@ describe('Content Type Middleware', () => {
       res.send()
     })
 
-    await request(app).get('/test_content_type_xml').expect('content-type', /xml/)
+    await request(app)
+      .get('/test_content_type_xml')
+      .expect('content-type', /xml/)
   })
 })
