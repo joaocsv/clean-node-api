3bbf5a1a416c0debccf93474ef6b7e33059ed82d
João Carlos <market4u.dev6@gmail.com>
2022-04-06 15:38:32 -0300

    refactor: move sut creation to a factory method

--- src/main/decorators/log.spec.ts
+++ src/main/decorators/log.spec.ts
@@ -1,25 +1,45 @@
 import { Controller, HttpRequest, HttpResponse } from '../../presentation/protocols'
 import { LogControllerDecorator } from './log'
 
+const makeControllerSub = (): Controller => {
+  class ControllerStub implements Controller {
+    async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
+      const httpResponse: HttpResponse = {
+        statusCode: 200,
+        body: {
+          id: 'any_id',
+          name: 'any_name',
+          email: 'any_email',
+          password: 'any_password'
+        }
+      }
+
+      return httpResponse
+    }
+  }
+
+  return new ControllerStub()
+}
+
+interface SutTypes {
+  sut: Controller
+  controllerStub: Controller
+}
+
+const makeSut = (): SutTypes => {
+  const controllerStub = makeControllerSub()
+  const logControllerDecorator = new LogControllerDecorator(controllerStub)
+
+  return {
+    sut: logControllerDecorator,
+    controllerStub: controllerStub
+  }
+}
+
 describe('LogController Decorator', () => {
   test('Should call controller handle', async () => {
-    class ControllerStub implements Controller {
-      async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-        const httpResponse: HttpResponse = {
-          statusCode: 200,
-          body: {
-            id: 'any_id',
-            name: 'any_name',
-            email: 'any_email',
-            password: 'any_password'
-          }
-        }
+    const { sut, controllerStub } = makeSut()
 
-        return httpResponse
-      }
-    }
-
-    const controllerStub = new ControllerStub()
     const controllerStubSpy = jest.spyOn(controllerStub, 'handle')
 
     const httpRequest: HttpRequest = {
@@ -31,9 +51,7 @@ describe('LogController Decorator', () => {
       }
     }
 
-    const logControllerDecorator = new LogControllerDecorator(controllerStub)
-
-    await logControllerDecorator.handle(httpRequest)
+    await sut.handle(httpRequest)
 
     expect(controllerStubSpy).toHaveBeenCalledWith(httpRequest)
   })
