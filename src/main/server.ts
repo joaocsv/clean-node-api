8f534dacd4f20e5b584e5fc38a528cca8c1fcb4c
Joao Carlos <joaoc.avils@gmail.com>
2022-04-19 17:00:04 -0300

    refactor: capture env port

--- src/main/server.ts
+++ src/main/server.ts
@@ -3,5 +3,5 @@ import { MongoHelper } from '../infra/db/mongodb/helpers/mongo-helper'
 
 MongoHelper.connect(env.mongoUrl).then(async () => {
   const app = (await import('./config/app')).default
-  app.listen(5050, () => console.log('Server running at http//localhost:5050'))
+  app.listen(env.port, () => console.log(`Server running at http//localhost:' ${env.port}`))
 }).catch(console.error)
